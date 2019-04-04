47173a8e97391e17507a6b593340864cab1c5000
Michael Verhulst <michael@terminallabs.com>
2019-04-03 23:15:30 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+yum -t install git
+
+mkdir -p /var/tmp/saltstack-cookiecutter
+
+cd /var/tmp/saltstack-cookiecutter
+
+git clone https://github.com/terminal-labs/sample-states.git
+
+mkdir -p /srv/salt/
+
+cp -r /var/tmp/saltstack-cookiecutter/sample-states/saltstack/states/* /srv/salt
