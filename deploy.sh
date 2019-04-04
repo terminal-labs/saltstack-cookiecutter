d7894a84034f2f1ff86d9d895f8c2cd276e49c69
Michael Verhulst <michael@terminallabs.com>
2019-04-03 23:46:57 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ cd /var/tmp/saltstack-cookiecutter
 
 git clone https://github.com/terminal-labs/sample-states.git
 
+cd sample-states
+
+git checkout testing
+
 mkdir -p /srv/salt/
 
 cp -r /var/tmp/saltstack-cookiecutter/sample-states/saltstack/states/* /srv/salt
