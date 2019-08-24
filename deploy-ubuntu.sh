8329f3a00689c52bc42c8e16046eb789b4bb5c23
Michael Verhulst <michael@terminallabs.com>
2019-08-24 16:11:50 -0500

    Create deploy-ubuntu.sh

---
+++ deploy-ubuntu.sh
@@ -0,0 +1,22 @@
+apt -t install git
+
+mkdir -p /var/tmp/saltstack-cookiecutter
+
+cd /var/tmp/saltstack-cookiecutter
+
+git clone https://github.com/terminal-labs/sample-states.git
+
+cd sample-states
+
+git checkout testing
+
+mkdir -p /srv
+mkdir -p /srv/pillar
+mkdir -p /srv/salt
+mkdir -p /srv/salt/_proxy
+mkdir -p /srv/salt/_modules
+mkdir -p /srv/salt/_grains
+
+cp -r /var/tmp/saltstack-cookiecutter/sample-states/saltstack/states/* /srv/salt
+
+rm -rf /var/tmp/saltstack-cookiecutter
