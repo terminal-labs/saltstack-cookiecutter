96d2d66f5aca8da8677a9965db6fa456fd7ad469
Michael Verhulst <michael@terminallabs.com>
2019-08-24 17:13:25 -0500

    Update deploy-centos.sh

--- deploy-centos.sh
+++ deploy-centos.sh
@@ -5,10 +5,12 @@ mkdir -p /var/tmp/saltstack-cookiecutter
 cd /var/tmp/saltstack-cookiecutter
 
 git clone https://github.com/terminal-labs/sample-states.git
+wget https://raw.githubusercontent.com/terminal-labs/saltstack-cookiecutter/master/master
+wget https://raw.githubusercontent.com/terminal-labs/saltstack-cookiecutter/master/minion
 
 cd sample-states
-
 git checkout testing
+cd ..
 
 mkdir -p /srv
 mkdir -p /srv/pillar
@@ -17,6 +19,13 @@ mkdir -p /srv/salt/_proxy
 mkdir -p /srv/salt/_modules
 mkdir -p /srv/salt/_grains
 
-cp -r /var/tmp/saltstack-cookiecutter/sample-states/saltstack/states/* /srv/salt
+mkdir -p /etc/salt
+mkdir -p /etc/salt/pki
+mkdir -p /etc/salt/pki/minion
+
+cp master /etc/salt/master
+cp minion /etc/salt/minion
+
+cp -r sample-states/saltstack/states/* /srv/salt
 
 rm -rf /var/tmp/saltstack-cookiecutter
