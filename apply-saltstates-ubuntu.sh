37dab83dcb18e6113b1d0259fb140d862dde09e5
Michael Verhulst <michael@terminallabs.com>
2020-01-12 23:20:16 -0600

    Update apply-saltstates-ubuntu.sh

--- apply-saltstates-ubuntu.sh
+++ apply-saltstates-ubuntu.sh
@@ -2,4 +2,7 @@ apt update
 apt install -y wget unzip
 wget https://raw.githubusercontent.com/terminal-labs/sample-states-installer/master/saltify-deploy-ubuntu.sh
 bash saltify-deploy-ubuntu.sh
-salt-call --local state.sls testing.helloworld
+cd saltstack
+cp -r states/* /srv/salt
+cp grains/grains /etc/salt/grains
+salt-call --local state.highstate --log-level info
